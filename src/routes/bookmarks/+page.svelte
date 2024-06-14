2ebe51bed7b06a4dedd1a4d5260b5eaa04ac42eb
Aeron <ajlaffere@gmail.com>
2024-06-14 15:53:08 +0100

    bookmarks page

--- src/routes/bookmarks/+page.svelte
+++ src/routes/bookmarks/+page.svelte
@@ -1,8 +1,74 @@
-<h3 class="text-lg font-semibold my-2">On living well</h3>
-<ul class="list-disc list-inside">
-<li><a href="https://www.ams.org/journals/notices/202309/noti2782/noti2782.html" class="underline text-blue-600">Disappointment</a> - Danny Calegari, a Professor of Mathematics at the University of Chicago reflects on disappointment.</li>
+<script lang="ts">
+    import { fly } from 'svelte/transition';
+</script>
+<div in:fly={{x: 10}}>
+<h3 class="my-2 text-lg font-semibold">On living well</h3>
+<ul class="list-inside list-disc">
+	<li>
+		<a
+			href="https://www.ams.org/journals/notices/202309/noti2782/noti2782.html"
+			class="text-blue-600 underline">Disappointment</a
+		> - Danny Calegari, a Professor of Mathematics at the University of Chicago reflects on disappointment.
+	</li>
 </ul>
-<h3 class="text-lg font-semibold my-2">Good websites</h3>
-<ul class="list-disc list-inside">
-<li><a href="https://patrickcollison.com/" class="underline text-blue-600">Patrick Collison's personal website</a></li>
+<h3 class="my-2 text-lg font-semibold">How to do science</h3>
+<ul class="list-inside list-disc">
+    <li>
+        <a href="https://ecommons.cornell.edu/items/f2e1c2d6-a811-4abf-914b-390561312956" class="text-blue-600 underline">The Incomplete Guide to the Art of Discovery
+        </a>
+    </li>
 </ul>
+<h3 class="my-2 text-lg font-semibold">How to make software</h3>
+<ul class="list-inside list-disc">
+	<li>
+		<a href="https://grugbrain.dev/" class="text-blue-600 underline">The Grug Brained Developer</a> -
+		The most important thing to realise about yourself is you're stupid and you should act accordingly.
+	</li>
+    <li>
+        <a href="https://danuker.go.ro/the-grand-unified-theory-of-software-architecture.html" class="text-blue-600 underline">The Grand Unified Theory of Software Architecture</a> - "The best software architecture is the one that allows you to change your mind.
+    </li>
+    <li>
+        <a href="https://www.cosmicpython.com/" class="text-blue-600 underline">Cosmic Python</a>
+    </li>
+    <li>
+        <a href="https://matthewstrom.com/writing/ui-density/" class="text-blue-600 underline">UI Density</a>
+    </li>
+    <li>
+        <a href="https://www.learnenough.com/text-editor-tutorial" class="text-blue-600 underline">Learn Enough Text Editor to Be Dangerous</a>
+    </li>
+</ul>
+<h3 class="my-2 text-lg font-semibold">The thing we call machine intelligence</h3>
+<ul class="list-inside list-disc">
+	<li>
+		<a href="https://theaisummer.com/diffusion-models/" class="text-blue-600 underline">
+			How diffusion models work: the math from scratch
+		</a>
+	</li>
+    <li>
+        <a href="https://github.com/adam-maj/deep-learning" class="text-blue-600 underline">
+            History of deep learning
+        </a>
+    </li>
+</ul>
+<h3 class="my-2 text-lg font-semibold">Accelerating progress</h3>
+<ul class="list-inside list-disc">
+    <li>
+        <a href="https://drydenbrown.posthaven.com/national-accelerationism" class="text-blue-600 underline">
+            National Accelerationism
+        </a>
+    </li>
+    <li>
+        <a href="https://vitalik.eth.limo/general/2023/11/27/techno_optimism.html" class="text-blue-600 underline">
+            My techno-optimism
+        </a>
+    </li>
+</ul>
+<h3 class="my-2 text-lg font-semibold">Good websites</h3>
+<ul class="list-inside list-disc">
+	<li>
+		<a href="https://patrickcollison.com/" class="text-blue-600 underline"
+			>Patrick Collison's personal website</a
+		>
+	</li>
+</ul>
+</div>
